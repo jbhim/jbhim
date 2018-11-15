6253afbd70e6a1282e83ca25bc8e84ccd2eb4403
jintao <jint@ywsoftware.com>
2018-11-15 19:07:25 +0800

    Activiti Config配置

--- src/main/java/com/amcjt/jbhim/ActivitiConfig.java
+++ src/main/java/com/amcjt/jbhim/ActivitiConfig.java
@@ -1,20 +1,21 @@
 package com.amcjt.jbhim;
 
-import org.activiti.engine.*;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.ResourcePatternResolver;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 
 @Configuration
 public class ActivitiConfig {
-    @Autowired
+    @Resource
     private DataSource dataSource;
-    @Autowired
+    @Resource
     private ResourcePatternResolver resourceLoader;
 
     /**
