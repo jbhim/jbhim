ac4385719bb803fd1def37f440ae8fd3f55aef54
jbhim <jt724721900@icloud.com>
2019-04-21 23:00:23 +0800

    跨域

--- src/main/java/com/amcjt/jbhim/model/MailBean.java
+++ src/main/java/com/amcjt/jbhim/model/MailBean.java
@@ -7,14 +7,25 @@ import java.util.Map;
 
 @Data
 public class MailBean implements Serializable {
-    //邮件接收人
+    /**
+     * 邮件接收人
+     */
     private String recipient;
-    //邮件主题
+    /**
+     * 邮件主题
+     */
     private String subject;
-    //邮件内容
+    /**
+     * 邮件内容
+     */
     private String content;
-    //邮件内容html
+    /**
+     * 邮件内容html
+     */
+
     private String htmlContent;
-    //附件 key: value => name: file/is
+    /**
+     * 附件 key: value => name: file/is
+     */
     private Map<String, Object> attachment;
 }
